553576ea57176c9a9af3a52052d7336ded753388
sukhi <meghalpu2017@gmail.com>
2020-04-17 22:40:39 +0530

    sukhoi

--- Techie.js
+++ Techie.js
@@ -3,7 +3,7 @@ const hbs = require('hbs');
 var app = express();
 hbs.registerPartials(__dirname + '/views/Partials1');
 app.set('view engine' , 'hbs');
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
 
